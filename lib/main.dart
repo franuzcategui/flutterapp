105d7c496ea15081af61fb90286276d88eaf21b2
franuzcategui <45088588+franuzcategui@users.noreply.github.com>
2021-08-11 13:57:06 -0600

    v1.2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       // title: 'Startup Name Generator',
+       title: 'Startup Name Generator',
+       theme: ThemeData(
+         primaryColor: Colors.cyanAccent,
+       ),
         home: RandomWords(),
     );
   }
